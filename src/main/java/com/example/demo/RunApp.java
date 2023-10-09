45eb0ff2ea7511151af6295fb1dc56da7cda8739
MissTracy <tlethoko021@student.wethinkcode.co.za>
2023-10-09 06:52:47 +0200

    finall

--- src/main/java/com/example/demo/RunApp.java
+++ src/main/java/com/example/demo/RunApp.java
@@ -1,15 +1,12 @@
 package com.example.demo;
 
 
-import io.swagger.annotations.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import org.springdoc.api.annotations.EnableOpenApi;
 
 
-//@Info(title = "Withdrawal app Documentation", version = "1.0")
 @EnableSwagger2
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.demo.repos")
